71e27a94aaa2f553cda87e37b3832f8ce3ba8925
GhostBat101 <77197454+GhostBat101@users.noreply.github.com>
2026-09-12 22:35:22 +0600

    config: configure PostCSS with Tailwind and Autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+/**
+ * PostCSS Configuration - Integrates Tailwind CSS and Autoprefixer build transformers.
+ * Communicates with: Vite CSS build pipeline and Tailwind CLI.
+ */
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+};
